60e0618459aee9683ac647306e9d8df55a542759
Feitoza de Azevedo <wrfv@gft.com>
2022-06-06 11:46:43 -0300

    formulario estendido exemplo

---
+++ src/app/pages/login/build-form.ts
@@ -0,0 +1,21 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface FormLoginModel {
+  username: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
+
+export class FormHelper {
+  protected form: FormGroup<FormLoginModel>;
+
+  constructor() {
+    this.formBuilder();
+  }
+
+  private formBuilder(): void {
+    this.form = new FormGroup<FormLoginModel>({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,58 +1,44 @@
-import { Component, OnDestroy, OnInit, Self } from '@angular/core';
+import { Component, OnDestroy, Self, SkipSelf } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginService } from './services/login.service';
 import { Subject, takeUntil } from 'rxjs';
-import { ResponseAuthParamsModel } from './models';
 import { LocalStorageService } from './services/local-storage.service';
 import { Router } from '@angular/router';
+import { FormHelper } from './build-form';
+import { AuthParamsModel, ResponseAuthParamsModel } from './models';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatInputModule],
+  imports: [HttpClientModule, FormsModule, ReactiveFormsModule, MatInputModule],
   providers: [LoginService, LocalStorageService],
-  standalone: true
+  standalone: true,
 })
-export class LoginComponent implements OnInit, OnDestroy {
-
-  public loginForm: FormGroup
-  private destroy$ = new Subject()
+export class LoginComponent extends FormHelper implements OnDestroy {
+  private destroy$ = new Subject();
 
   constructor(
-    private formBuilder: FormBuilder,
-    private Router: Router,
     @Self() private loginService: LoginService,
-    @Self() private LocalStorageService: LocalStorageService) { }
-
-  ngOnInit(): void {
-    this.loginForm = this.createForm()
+    @Self() private LocalStorageService: LocalStorageService,
+    @SkipSelf() private Router: Router
+  ) {
+    super();
   }
-
   public authentication(): void {
-    this.loginService.auth(this.loginForm.value)
+    this.loginService
+      .auth(this.form.value as AuthParamsModel)
       .pipe(takeUntil(this.destroy$))
       .subscribe((tokenUser: ResponseAuthParamsModel) => {
         this.LocalStorageService.setItem(tokenUser);
-        this.Router.navigate(['home'])
-      })
-  }
-
-  private createForm(): FormGroup {
-    return this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+        this.Router.navigate(['home']);
+      });
   }
 
   ngOnDestroy(): void {
-    this.destroy$.next(null)
-    this.destroy$.complete()
+    this.destroy$.next(null);
+    this.destroy$.complete();
   }
 }
